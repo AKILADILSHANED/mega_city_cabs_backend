6c10ab29d7d9bf91e50209c0875662b1ea9363cf
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-03-01 00:26:26 +0530

    Modified adminServiceIMPL

--- src/main/java/com/mega_city_cabs/mega_city_cabs/Service/adminServiceIMPL.java
+++ src/main/java/com/mega_city_cabs/mega_city_cabs/Service/adminServiceIMPL.java
@@ -1,7 +1,7 @@
 package com.mega_city_cabs.mega_city_cabs.Service;
 
-import com.mega_city_cabs.mega_city_cabs.DTO.registerVehicleDTO;
 import com.mega_city_cabs.mega_city_cabs.Repository.adminRepo;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +11,19 @@ public class adminServiceIMPL implements adminService {
     @Autowired
     adminRepo login;
 
+    @Autowired
+    HttpSession session;
+
     @Override
     public boolean adminLogin(String userName, String password) {
-        String loginId = userName;
-        String pssword = password;
-        return login.adminExistance(userName, password);
-    }
 
+        boolean adminAvailability = login.adminExistance(userName, password);
+        if(adminAvailability){
+            String adminId = login.getAdminId(userName, password);
+            session.setAttribute("admin_id", adminId);
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
