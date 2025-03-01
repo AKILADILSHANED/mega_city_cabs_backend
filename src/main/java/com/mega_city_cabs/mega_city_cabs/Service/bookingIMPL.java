6fd4c379a9097e393008ba61aeebed89e775eb3b
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-03-02 00:12:20 +0530

    Implemented booking IMPL

--- src/main/java/com/mega_city_cabs/mega_city_cabs/Service/bookingIMPL.java
+++ src/main/java/com/mega_city_cabs/mega_city_cabs/Service/bookingIMPL.java
@@ -1,5 +1,6 @@
 package com.mega_city_cabs.mega_city_cabs.Service;
 
+import com.mega_city_cabs.mega_city_cabs.DTO.approveBookingDTO;
 import com.mega_city_cabs.mega_city_cabs.DTO.bookingHistoryDTO;
 import com.mega_city_cabs.mega_city_cabs.DTO.cancelBookingDTO;
 import com.mega_city_cabs.mega_city_cabs.DTO.newBookingDTO;
@@ -7,6 +8,7 @@ import com.mega_city_cabs.mega_city_cabs.Entity.booking;
 import com.mega_city_cabs.mega_city_cabs.Repository.adminRepo;
 import com.mega_city_cabs.mega_city_cabs.Repository.bookingRepo;
 import com.mega_city_cabs.mega_city_cabs.Repository.customerRepo;
+import com.mega_city_cabs.mega_city_cabs.SqlMappers.bookingApprovalMapper;
 import com.mega_city_cabs.mega_city_cabs.SqlMappers.bookingHistoryMapper;
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
@@ -162,5 +164,11 @@ public class bookingIMPL implements customerBooking {
         String sessionCusId = session.getAttribute("customer_id").toString();
         String Sql = "SELECT b.booking_id AS 'Booking ID', b.booking_date AS 'Date', b.booking_type AS 'Type', b.pickup_location AS 'Pick Up', b.destination AS 'Destination', COALESCE(V.vehicle_number, 'No Vehicle assigned') AS 'Vehicle', COALESCE(DV.first_name, 'Driver not assigned') AS 'Driver' FROM booking b LEFT JOIN vehicle_assignment VS ON b.booking_id = VS.booking_id LEFT JOIN vehicle V ON VS.vehicle_id = V.vehicle_id LEFT JOIN driver_assignment DS ON b.booking_id = DS.booking_id LEFT JOIN driver DV ON DS.driver_id = DV.driver_id WHERE b.customer_id = ?";
         return template.query(Sql,new bookingHistoryMapper(),new Object[]{sessionCusId});
+    }
+
+    @Override
+    public List<approveBookingDTO> approveBookings() {
+        String Sql = "SELECT b.booking_id AS 'Booking ID', b.booking_date AS 'Date', b.booking_type AS 'Type', b.pickup_location AS 'Pick Up', b.destination AS 'Destination', COALESCE(V.vehicle_number, 'No Vehicle assigned') AS 'Vehicle', COALESCE(DV.first_name, 'Driver not assigned') AS 'Driver', Cus.first_name AS 'Customer First Name', Cus.last_name AS 'Customer Last Name', Cus.contact AS 'Contact' FROM booking b LEFT JOIN vehicle_assignment VS ON b.booking_id = VS.booking_id LEFT JOIN vehicle V ON VS.vehicle_id = V.vehicle_id LEFT JOIN driver_assignment DS ON b.booking_id = DS.booking_id LEFT JOIN driver DV ON DS.driver_id = DV.driver_id LEFT JOIN customer Cus ON b.customer_id = Cus.customer_id WHERE b.is_cancelled = 0 AND b.approved_by IS NULL";
+        return template.query(Sql, new bookingApprovalMapper());
     }
 }
