09ac84e01a36ef6cb57afdef8673f0a98ab8f742
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2025-03-08 14:55:40 +0530

    src/main/java/com/mega_city_cabs/mega_city_cabs/Service/receiptService.java

--- src/main/java/com/mega_city_cabs/mega_city_cabs/Service/receiptService.java
+++ src/main/java/com/mega_city_cabs/mega_city_cabs/Service/receiptService.java
@@ -1,11 +1,14 @@
 package com.mega_city_cabs.mega_city_cabs.Service;
 
 import com.mega_city_cabs.mega_city_cabs.DTO.bookingDetailsForReceipt;
+import com.mega_city_cabs.mega_city_cabs.DTO.receiptConfirmDTO;
 import com.mega_city_cabs.mega_city_cabs.DTO.receiptDTO;
+import com.mega_city_cabs.mega_city_cabs.DTO.receiptPrintDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface receiptService {
-    public String issueReceipt(receiptDTO receiptDto);
+    public receiptConfirmDTO issueReceipt(receiptDTO receiptDto);
     public bookingDetailsForReceipt getBookingDetailsForReceipt(String bookingId);
+    public receiptPrintDTO getReceiptDetails(String receiptNumber, int vat);
 }
